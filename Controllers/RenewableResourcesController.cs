754ff64bc514a777c7b4435f2ea87a0be7d1b8a3
nfalak-9 <nfalak@usf.edu>
2024-04-23 18:37:32 -0400

    Renewable Resource Added

---
+++ Controllers/RenewableResourcesController.cs
@@ -0,0 +1,73 @@
+﻿using Energy_Insights.Models;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Energy_Insights.Controllers
+{
+    public class RenewableResourcesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var energyDataList = new List<RenewableResourcesModel>
+            {
+                new RenewableResourcesModel
+                {
+                    Country = "USA",
+                    EnergyTypes = new Dictionary<string, string>
+                    {
+                        { "Solar", "100 GW" },
+                        { "Wind", "150 GW" },
+                        { "Hydro", "80 GW" }
+                    }
+                },
+                new RenewableResourcesModel
+                {
+                    Country = "India",
+                    EnergyTypes = new Dictionary<string, string>
+                    {
+                        { "Solar", "50 GW" },
+                        { "Wind", "60 GW" },
+                        { "Hydro", "45 GW" },
+                        { "Biomass", "10 GW" }
+                    }
+                },
+                new RenewableResourcesModel
+                {
+                    Country = "China",
+                    EnergyTypes = new Dictionary<string, string>
+                    {
+                        { "Solar", "200 GW" },
+                        { "Wind", "300 GW" },
+                        { "Hydro", "250 GW" },
+                        { "Biomass", "50 GW" }
+                    }
+                },
+                new RenewableResourcesModel
+                {
+                    Country = "Russia",
+                    EnergyTypes = new Dictionary<string, string>
+                    {
+                        { "Hydro", "30 GW" },
+                        { "Wind", "5 GW" },
+                        { "Biomass", "4 GW" }
+                    }
+                },
+                new RenewableResourcesModel
+                {
+                    Country = "Germany",
+                    EnergyTypes = new Dictionary<string, string>
+                    {
+                        { "Solar", "60 GW" },
+                        { "Wind", "70 GW" },
+                        { "Biomass", "20 GW" },
+                        { "Hydro", "10 GW" }
+                    }
+                }
+            };
+
+            ViewBag.LogoPath = "~/img/renew.jpeg"; 
+            return View("RenewableResources", energyDataList);
+        }
+    }
+}
+
---
+++ Models/RenewableResourcesModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Energy_Insights.Models
+{
+    public class RenewableResourcesModel
+    {
+        public string Country { get; set; } = string.Empty;
+        public Dictionary<string, string> EnergyTypes { get; set; } = new Dictionary<string, string>();
+    }
+}

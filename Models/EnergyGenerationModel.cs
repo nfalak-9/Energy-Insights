77fee98a174ae2d64556e679433b5e89f821a42f
amishabt <amishab@usf.edu>
2024-04-26 17:19:11 -0400

    added validation, layout and nav

--- Models/EnergyGenerationModel.cs
+++ Models/EnergyGenerationModel.cs
@@ -1,12 +1,17 @@
-﻿using System.Diagnostics.Metrics;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Metrics;
 
 namespace Energy_Insights.Models
 {
     public class EnergyGenerationModel
     {
         public int Id { get; set; }
+        [Required]
         public int CountryId { get; set; }
+        [Required]
         public int EnergyTypeId { get; set; }
+        [Required]
+        [DataType("double")]
         public double Amount { get; set; }
 
         // Navigation properties

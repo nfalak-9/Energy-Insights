d00c44168b1a3f87aaf50943b0656b7fdfa0b0ce
Venkata Dharma Teja Konakanchi <vk35@usf.edu>
2024-04-25 22:40:39 -0400

    Articles page added

---
+++ Controllers/ArticlesController.cs
@@ -0,0 +1,19 @@
+﻿using Energy_Insights.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Energy_Insights.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var articles = new List<Articles>
+            {
+                new Articles { Title = "The Future of Solar Energy", Content = "As we step into 2024, the solar energy landscape is poised for unprecedented growth and innovation...", Link = "https://www.nexamp.com/blog/the-future-of-solar-energy-trends-and-predictions-2024" },
+                // Add more articles as needed
+            };
+            ViewBag.LogoPath = "~/img/logo.png";
+            return View(articles);
+        }
+    }
+}
---
+++ Models/Articles.cs
@@ -0,0 +1,11 @@
+﻿namespace Energy_Insights.Models
+{
+    public class Articles
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Link { get; set; }
+
+       
+    }
+}

7c31a86678e5d1e8059e025101ed10a1af836e2a
nfalak-9 <nfalak@usf.edu>
2024-04-26 18:29:27 -0400

    Changed Data Model

---
+++ Migrations/20240426015545_InitialCreateUpdate-Database.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Energy_Insights.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreateUpdateDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
